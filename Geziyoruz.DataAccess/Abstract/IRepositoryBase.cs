bb8eb6bcfc01b129c30d0e124b33c5176343be25
ebarut7 <emrebarut1997@gmail.com>
2024-02-29 17:21:49 +0300

    #1 DataAccess katmanı açıldı

---
+++ Geziyoruz.DataAccess/Abstract/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Geziyoruz.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace Geziyoruz.DataAccess.Abstract
+{
+    public interface IRepositoryBase<T> where T : class, IEntity, new()
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> expression);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression = null);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
