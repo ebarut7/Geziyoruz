c01f50a8a425d81a8f53f7748f2daddd0eaf00f3
ebarut7 <emrebarut1997@gmail.com>
2024-03-10 00:47:22 +0300

    #1 Authcontroller açıldı

---
+++ Geziyoruz.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Geziyoruz.Business.Abstract;
+using Geziyoruz.Entities.Concrete.Dtos.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geziyoruz.WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto loginDto) 
+        {
+            Microsoft.AspNetCore.Identity.SignInResult response = await _authService.LoginAsync(loginDto);
+            if(response.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Register(CustomerRegisterDto customerRegisterDto) 
+        {
+            bool response = (await _authService.CustomerRegisterAsync(customerRegisterDto)).Succeeded;
+            return RedirectToAction("Home", "Home");
+        }
+    }
+}
---
+++ Geziyoruz.WebUI/wwwroot/js/login.js
@@ -0,0 +1,14 @@
+﻿const signInBtn = document.getElementById('signInBtn');
+const signUpBtn = document.getElementById('signUpBtn');
+const signInForm = document.querySelector('.sign-in-form');
+const signUpForm = document.querySelector('.sign-up-form');
+
+signInBtn.addEventListener('click', () => {
+    signInForm.classList.add('active');
+    signUpForm.classList.remove('active');
+});
+
+signUpBtn.addEventListener('click', () => {
+    signInForm.classList.remove('active');
+    signUpForm.classList.add('active');
+});
